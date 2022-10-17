69ead6770c6d1460d1466e6b92eddbf1ef72bc39
andrew-hardy <aha@cluedin.com>
2022-10-17 12:49:28 +1000

    chore: Add integration tests (#12)

--- src/Connector.AzureDataLake/Connector/IAzureDataLakeClient.cs
+++ src/Connector.AzureDataLake/Connector/IAzureDataLakeClient.cs
@@ -6,6 +6,6 @@ namespace CluedIn.Connector.AzureDataLake.Connector
     public interface IAzureDataLakeClient
     {
         Task<DataLakeDirectoryClient> EnsureDataLakeDirectoryExist(AzureDataLakeConnectorJobData configuration);
-        Task SaveData(AzureDataLakeConnectorJobData configuration, string content);
+        Task SaveData(AzureDataLakeConnectorJobData configuration, string content, string fileName);
     }
 }
