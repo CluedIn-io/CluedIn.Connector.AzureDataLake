e68a59efbf2ad43870fe10526f320ad3b977c29d
Ho Peng Foong <pfh@cluedin.com>
2024-06-21 18:07:22 +0800

    feat: Use custom scheduler for jobs (#44)

--- src/Connector.AzureDataLake/Connector/AzureDataLakeExportEntitiesJob.cs
+++ src/Connector.AzureDataLake/Connector/AzureDataLakeExportEntitiesJob.cs
@@ -24,7 +24,7 @@ internal class AzureDataLakeExportEntitiesJob : AzureDataLakeJobBase
     {
     }
 
-    protected override async Task DoRunAsync(ExecutionContext context, JobArgs args)
+    public override async Task DoRunAsync(ExecutionContext context, JobArgs args)
     {
         using var exportJobLoggingScope = context.Log.BeginScope(new Dictionary<string, object>
         {
@@ -33,13 +33,7 @@ internal class AzureDataLakeExportEntitiesJob : AzureDataLakeJobBase
             ["ExportJob"] = nameof(AzureDataLakeExportEntitiesJob),
         });
         context.Log.LogDebug("Begin export entities job '{ExportJob}' for '{StreamId}' using {Schedule}.", nameof(AzureDataLakeExportEntitiesJob), args.Message, args.Schedule);
-
-        if (args.Schedule == AzureDataLakeConstants.CronSchedules[AzureDataLakeConstants.JobScheduleNames.Never])
-        {
-            context.Log.LogDebug("Job is disabled because cron is set to {CronSchedule}. Skipping export.", AzureDataLakeConstants.JobScheduleNames.Never);
-            return;
-        }
-
+                
         var streamRepository = context.ApplicationContext.Container.Resolve<IStreamRepository>();
         var client = context.ApplicationContext.Container.Resolve<IAzureDataLakeClient>();
         var organizationProviderDataStore = context.Organization.DataStores.GetDataStore<ProviderDefinition>();
@@ -102,7 +96,7 @@ internal class AzureDataLakeExportEntitiesJob : AzureDataLakeJobBase
             return;
         }
 
-        var asOfTime = GetLastOccurence(args, configuration);
+        var asOfTime = GetLastOccurence(context, args, configuration);
         var outputFormat = configuration.OutputFormat.ToLowerInvariant();
         var outputFileName = GetOutputFileName(streamId, asOfTime, outputFormat);
 
@@ -155,10 +149,12 @@ internal class AzureDataLakeExportEntitiesJob : AzureDataLakeJobBase
         return outputFormat;
     }
 
-    private static DateTime GetLastOccurence(JobArgs args, AzureDataLakeConnectorJobData configuration)
+    private static DateTime GetLastOccurence(ExecutionContext context, JobArgs args, AzureDataLakeConnectorJobData configuration)
     {
-        if (configuration.UseCurrentTimeForExport)
+        if (configuration.UseCurrentTimeForExport
+            || args.Schedule == AzureDataLakeConstants.CronSchedules[AzureDataLakeConstants.JobScheduleNames.Never])
         {
+            context.Log.LogDebug("Using current time for export.");
             return DateTime.UtcNow;
         }
 
