b1369a976587608e61ff60dfdd6a3eb2c4075190
Richard Naparite <chad.naparite@gmail.com>
2024-10-14 15:55:53 +0800

    update the assert value

--- test/integration/Connector.AzureDataLake.Tests.Integration/AzureDataLakeConnectorTests.cs
+++ test/integration/Connector.AzureDataLake.Tests.Integration/AzureDataLakeConnectorTests.cs
@@ -1060,18 +1060,18 @@ namespace CluedIn.Connector.AzureDataLake.Tests.Integration
             ContainerName test
             EntityType /Person
             Epoch 1724192160000
-            IncomingEdges [{"FromReference":{"Code":{"Origin":{"Code":"Acceptance","Id":null},"Value":"7c5591cf-861a-4642-861d-3b02485854a0","Key":"/Person#Acceptance:7c5591cf-861a-4642-861d-3b02485854a0","Type":{"IsEntityContainer":false,"Root":null,"Code":"/Person"}},"Type":{"IsEntityContainer":false,"Root":null,"Code":"/Person"},"Name":null,"Properties":null,"PropertyCount":null,"EntityId":null,"IsEmpty":false},"ToReference":{"Code":{"Origin":{"Code":"Somewhere","Id":null},"Value":"1234","Key":"/EntityA#Somewhere:1234","Type":{"IsEntityContainer":false,"Root":null,"Code":"/EntityA"}},"Type":{"IsEntityContainer":false,"Root":null,"Code":"/EntityA"},"Name":null,"Properties":null,"PropertyCount":null,"EntityId":null,"IsEmpty":false},"EdgeType":{"Root":null,"Code":"/EntityA"},"HasProperties":false,"Properties":{},"CreationOptions":0,"Weight":null,"Version":0}]
+            IncomingEdges ["EdgeType: /EntityA; From: 低:/Person#Acceptance:7c5591cf-861a-4642-861d-3b02485854a0; To: 低:/EntityA#Somewhere:1234; Properties: 0"]
             Name Jean Luc Picard
             OriginEntityCode /Person#Acceptance:7c5591cf-861a-4642-861d-3b02485854a0
-            OutgoingEdges [{"FromReference":{"Code":{"Origin":{"Code":"Somewhere","Id":null},"Value":"5678","Key":"/EntityB#Somewhere:5678","Type":{"IsEntityContainer":false,"Root":null,"Code":"/EntityB"}},"Type":{"IsEntityContainer":false,"Root":null,"Code":"/EntityB"},"Name":null,"Properties":null,"PropertyCount":null,"EntityId":null,"IsEmpty":false},"ToReference":{"Code":{"Origin":{"Code":"Acceptance","Id":null},"Value":"7c5591cf-861a-4642-861d-3b02485854a0","Key":"/Person#Acceptance:7c5591cf-861a-4642-861d-3b02485854a0","Type":{"IsEntityContainer":false,"Root":null,"Code":"/Person"}},"Type":{"IsEntityContainer":false,"Root":null,"Code":"/Person"},"Name":null,"Properties":null,"PropertyCount":null,"EntityId":null,"IsEmpty":false},"EdgeType":{"Root":null,"Code":"/EntityB"},"HasProperties":false,"Properties":{},"CreationOptions":0,"Weight":null,"Version":0}]
+            OutgoingEdges ["EdgeType: /EntityB; From: 低:/EntityB#Somewhere:5678; To: 低:/Person#Acceptance:7c5591cf-861a-4642-861d-3b02485854a0; Properties: 0"]
             PersistHash etypzcezkiehwq8vw4oqog==
             PersistVersion 1
             ProviderDefinitionId c444cda8-d9b5-45cc-a82d-fef28e08d55c
             Timestamp 2024-08-21T03:16:00.0000000+05:00
-            user.age 123
-            user.dobInDateTime 2000-01-02T03:04:05
-            user.dobInDateTimeOffset 2000-01-02T03:04:05+12:34
-            user.lastName Picard
+            user_age 123
+            user_dobInDateTime 2000-01-02T03:04:05
+            user_dobInDateTimeOffset 2000-01-02T03:04:05+12:34
+            user_lastName Picard
 
             """, sb.ToString());
 
