ddf966b1efca16cdaa118b7d7fbe158771fd1674
Ho Peng Foong <pfh@cluedin.com>
2025-04-25 12:10:37 +0800

    feat: Open Mirroring

---
+++ src/Connector.FabricOpenMirroring/OpenMirroringConnectorJobData.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+
+using CluedIn.Connector.DataLake.Common;
+
+namespace CluedIn.Connector.FabricOpenMirroring;
+
+internal class OpenMirroringConnectorJobData : DataLakeJobData
+{
+    public OpenMirroringConnectorJobData(
+        IDictionary<string, object> configurations,
+        string containerName = null)
+        : base(configurations, containerName)
+    {
+    }
+
+    public string WorkspaceName => GetConfigurationValue(OpenMirroringConstants.WorkspaceName) as string;
+    public string MirroredDatabaseName => GetConfigurationValue(OpenMirroringConstants.MirroredDatabaseName) as string ?? string.Empty;
+    public string ClientId => GetConfigurationValue(OpenMirroringConstants.ClientId) as string;
+    public string ClientSecret => GetConfigurationValue(OpenMirroringConstants.ClientSecret) as string;
+    public string TenantId => GetConfigurationValue(OpenMirroringConstants.TenantId) as string;
+    public override bool IsStreamCacheEnabled => true;
+    public override bool ShouldWriteGuidAsString => true;
+    public override bool ShouldEscapeVocabularyKeys => true;
+    public override bool IsDeltaMode => true;
+    public override bool IsOverwriteEnabled => false;
+    public override bool IsArrayColumnsEnabled => false;
+    public virtual bool ShouldCreateMirroredDatabase => GetConfigurationValue(OpenMirroringConstants.ShouldCreateMirroredDatabase) as bool? ?? false;
+
+    protected override void AddToHashCode(HashCode hash)
+    {
+        hash.Add(WorkspaceName);
+        hash.Add(MirroredDatabaseName);
+        hash.Add(ClientId);
+        hash.Add(ClientSecret);
+        hash.Add(TenantId);
+        hash.Add(ShouldCreateMirroredDatabase);
+
+        base.AddToHashCode(hash);
+    }
+
+    public override bool Equals(object obj)
+    {
+        return Equals(obj as OpenMirroringConnectorJobData);
+    }
+
+    public bool Equals(OpenMirroringConnectorJobData other)
+    {
+        return other != null &&
+            WorkspaceName == other.WorkspaceName &&
+            MirroredDatabaseName == other.MirroredDatabaseName &&
+            ClientId == other.ClientId &&
+            ClientSecret == other.ClientSecret &&
+            TenantId == other.TenantId &&
+            ShouldCreateMirroredDatabase == other.ShouldCreateMirroredDatabase &&
+            base.Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+}
---
+++ src/Connector.FabricOpenMirroring/OpenMirroringJobDataFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using CluedIn.Connector.DataLake.Common;
+using CluedIn.Core;
+
+namespace CluedIn.Connector.FabricOpenMirroring;
+
+public class OpenMirroringJobDataFactory : DataLakeJobDataFactoryBase, IDataLakeJobDataFactory
+{
+    protected override Task<IDataLakeJobData> CreateJobData(
+        ExecutionContext executionContext,
+        IDictionary<string, object> authenticationDetails,
+        string containerName)
+    {
+        return Task.FromResult<IDataLakeJobData>(new OpenMirroringConnectorJobData(authenticationDetails, containerName));
+    }
+
+    public override async Task<IDataLakeJobData> GetConfiguration(ExecutionContext executionContext, IDictionary<string, object> authenticationDetails, string containerName = null)
+    {
+        if (!authenticationDetails.TryGetValue(OpenMirroringConstants.MirroredDatabaseName, out var value)
+            || string.IsNullOrWhiteSpace(value?.ToString()))
+        {
+            if (authenticationDetails.TryGetValue(DataLakeConstants.ProviderDefinitionIdKey, out var providerDefinitionId))
+            {
+                authenticationDetails[OpenMirroringConstants.MirroredDatabaseName] = $"CluedIn_ExportTarget_{providerDefinitionId:N}";
+                //authenticationDetails[OpenMirroringConstants.ShouldCreateMirroredDatabase] = true;
+            }
+        }
+
+        return await base.GetConfiguration(executionContext, authenticationDetails, containerName);
+    }
+}
