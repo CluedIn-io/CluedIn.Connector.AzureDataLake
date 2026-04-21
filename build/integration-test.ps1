09bb72b3ea57a592105368ffc48e10639395856a
Ho Peng Foong <pfh@cluedin.com>
2026-04-21 22:45:49 +0800

    fix flush

--- build/integration-test.ps1
+++ build/integration-test.ps1
@@ -66,9 +66,6 @@ function Run-Setup() {
 	$createDatabaseCommand = "CREATE DATABASE [$databaseName]"
 	Write-Host "##[command]docker exec $containerName /opt/mssql-tools18/bin/sqlcmd -S $databaseHost -U $databaseUser -P $databasePassword -C -Q $createDatabaseCommand"
 	docker exec $containerName /opt/mssql-tools18/bin/sqlcmd -S $databaseHost -U $databaseUser -P $databasePassword -C -Q $createDatabaseCommand
-	Get-Location
-	dotnet test --blame-hang-timeout 2m --blame --blame-hang-dump-type full --diag diagnosticlog.txt ./test/integration/Connector.AmazonS3.Tests.Integration/Connector.AmazonS3.Tests.Integration.csproj --configuration Release --no-restore --no-build
-	Get-Content "diagnosticlog.txt" | Write-Host
 }
 
 function Run-TearDown() {
