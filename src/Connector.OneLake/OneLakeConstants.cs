00a2a4ce7626aabcb03c8f9a1c7adc9448ed5cb5
Ho Peng Foong <pfh@cluedin.com>
2026-04-23 11:48:15 +0800

    feat: Workspace level private link

--- src/Connector.OneLake/OneLakeConstants.cs
+++ src/Connector.OneLake/OneLakeConstants.cs
@@ -19,6 +19,7 @@ public class OneLakeConstants : DataLakeConstants, IOneLakeConstants
     public const string ClientSecret = nameof(ClientSecret);
     public const string TenantId = nameof(TenantId);
     public const string ShouldLoadToTable = nameof(ShouldLoadToTable);
+    public const string UseWorkspaceLevelPrivateLink = nameof(UseWorkspaceLevelPrivateLink);
     public const string TableName = nameof(TableName);
 
     public OneLakeConstants(ApplicationContext applicationContext) : base(DataLakeProviderId,
@@ -201,6 +202,18 @@ public class OneLakeConstants : DataLakeConstants, IOneLakeConstants
                         UnfulfilledAction = ControlDependencyUnfulfilledAction.Hidden,
                     },
                 },
+            });
+        controls.Add(
+            new()
+            {
+                Name = UseWorkspaceLevelPrivateLink,
+                DisplayName = "Use workspace level private link",
+                Type = "checkbox",
+                Help = """
+                       When enabled, connection to fabric will use workspace level URLs.
+                       Private links should be setup according to https://learn.microsoft.com/en-us/fabric/security/security-workspace-level-private-links-set-up?tabs=fabric-portal
+                       """,
+                IsRequired = false,
             });
         return new AuthMethods
         {
