3532febeab1b7aca2ed4b8a1878792caa860b2c2
Ho Peng Foong <pfh@cluedin.com>
2026-05-11 10:54:49 +0800

    fix: Prevent missing registration by always registering and using lock to prevent duplicate registration 4.6 (#186)
    * fix: Prevent missing registration by always registering and using lock to prevent duplicate registraiton
    
    * doc: Release notes

--- src/Connector.DataLake.Common/InstallComponentsBase.cs
+++ src/Connector.DataLake.Common/InstallComponentsBase.cs
@@ -1,4 +1,4 @@
-﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
 
@@ -10,6 +10,9 @@ namespace CluedIn.Connector.DataLake.Common;
 
 internal abstract class InstallComponentsBase : IWindsorInstaller
 {
+    private static readonly object _extendedConfigurationRegistrationLock = new();
+    private static bool _isExtendedConfigurationRegistered;
+
     public abstract void Install(IWindsorContainer container, IConfigurationStore store);
 
     protected static void DefaultInstall<TExportJob, TClient, TIConstants, TConstants, TJobDataFactory>(IWindsorContainer container, IConfigurationStore store)
@@ -24,6 +27,13 @@ internal abstract class InstallComponentsBase : IWindsorInstaller
         container.Register(Component.For<TIConstants>().ImplementedBy<TConstants>().LifestyleSingleton());
         container.Register(Component.For<TJobDataFactory>().ImplementedBy<TJobDataFactory>().LifestyleSingleton());
 
-        container.Register(Component.For<IExtendedConfigurationProvider>().ImplementedBy<DataLakeExtendedConfigurationProvider>().LifestyleSingleton().OnlyNewServices());
+        lock (_extendedConfigurationRegistrationLock)
+        {
+            if (!_isExtendedConfigurationRegistered)
+            {
+                container.Register(Component.For<IExtendedConfigurationProvider>().ImplementedBy<DataLakeExtendedConfigurationProvider>().LifestyleSingleton());
+                _isExtendedConfigurationRegistered = true;
+            }
+        }
     }
 }
